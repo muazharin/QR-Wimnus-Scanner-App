6aa458a0a8e4bfee894f8b006ab39f011cd2e3a4
root <alfanmuazharin@gmail.com>
2019-01-02 23:26:23 +0800

    halaman scanner qr wimnus scanner

---
+++ app/src/main/java/com/example/muaz/qrcodescanner/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.muaz.qrcodescanner;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import java.util.HashMap;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView zXingScannerView;
+    private Vibrator v;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                generate();
+
+            } else {
+                generate();
+            }
+        }
+
+    }
+
+    private void generate(){
+        zXingScannerView = new ZXingScannerView(this);
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(final Result result) {
+        zXingScannerView.stopCamera();
+
+        v.vibrate(100);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Participant data found!");
+        alertDialogBuilder
+                .setMessage("\nzHx"+result.getText()+"codeXV\n")
+                .setCancelable(false)
+                .setPositiveButton("Save",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        pesertaTambah(result);
+                        finish();
+                    }
+                })
+                .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //generate();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
+    }
+
+    private void pesertaTambah(final Result result) {
+        final String idPes = result.getText();
+
+        class PesertaTambah extends AsyncTask<Void, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this, "Adding..","Wait..",false,false);
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put(Konfigurasi.KEY_PES_ID,idPes);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Konfigurasi.URL_ADD, params);
+                return res;
+            }
+        }
+
+        PesertaTambah pt = new PesertaTambah();
+        pt.execute();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                generate();
+            }else{
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(getIntent());
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+}
