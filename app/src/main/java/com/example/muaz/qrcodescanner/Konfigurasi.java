17bef15c29631cb01be086679478447bbec77958
root <alfanmuazharin@gmail.com>
2019-01-02 23:30:12 +0800

    halaman konfigurasi dan koneksi qr wimnus scanner app dengan qr wimnus scanner web admin

---
+++ app/src/main/java/com/example/muaz/qrcodescanner/Konfigurasi.java
@@ -0,0 +1,16 @@
+package com.example.muaz.qrcodescanner;
+
+public class Konfigurasi {
+
+    //PENTING! JANGAN LUPA GANTI IP SESUAI DENGAN IP KOMPUTER DIMANA DATA PHP BERADA
+    public static final String URL_ADD="http://192.168.43.2/peserta/pesertaHadir.php";
+
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Skrip PHP
+    public static final String KEY_PES_ID = "idPes";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "idPes";
+
+    public static final String PES_ID = "pes_id";
+}
